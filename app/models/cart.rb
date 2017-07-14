589b097af54b84aaab3fb92acc580884b02b084b
r0tiart <antley.chu@gmail.com>
2017-07-14 17:08:12 -0400

    added total and add_item methods

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,18 @@ class Cart < ActiveRecord::Base
 	has_many :line_items
 	has_many :items, through: :line_items
 
+	def total
+		total = line_items.collect{ |lineitem| lineitem.item.price * lineitem.quantity}.sum
+	end
+
+	def add_item(item_id)
+		if line_items.exists?(item_id: item_id)
+			line = line_items.where(item_id: 1).first
+			line.quantity += 1
+			line
+		else
+			self.line_items.build(item_id: item_id)
+		end
+	end
+
 end
