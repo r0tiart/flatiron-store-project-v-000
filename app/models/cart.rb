8e115d952329e135791af128e6bc7aa0ebb6e297
r0tiart <antley.chu@gmail.com>
2017-07-14 13:36:30 -0400

    fixed tables and added assocations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,6 @@
 class Cart < ActiveRecord::Base
+	belongs_to :user
+	has_many :line_items
+	has_many :items, through: :line_items
 
 end
--- db/migrate/20140805183012_create_items.rb
+++ db/migrate/20140805183012_create_items.rb
@@ -1,5 +1,11 @@
 class CreateItems < ActiveRecord::Migration
   def change
-
+  	create_table :items do |t|
+  		t.string :title
+  		t.integer :price
+  		t.string :description
+  		t.integer :inventory
+  		t.integer :category_id
+  	end
   end
 end
