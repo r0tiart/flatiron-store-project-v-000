34809ae17fe230772b0b66a5a0ae4b4b5ad5cec7
r0tiart <antley.chu@gmail.com>
2017-07-17 18:52:34 -0400

    added an edit button to go back to cart after clicking checkout

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,23 @@
 class CartsController < ApplicationController
 	def show
-		if !!session[:cart_id]
+		if !!current_user.current_cart
 			@cart = Cart.find_by(id: params[:id])
 		else
 			redirect_to "/"
 		end
+	end
+
+	def checkout
+		@cart = Cart.find_by(id: params[:id])
+		@cart.status = "submitted"
+		@cart.save
+		redirect_to cart_path(@cart)
+	end
+
+	def update 
+		@cart = Cart.find_by(id: params[:id])
+		@cart.status = "new"
+		@cart.save
+		redirect_to cart_path
 	end
 end
